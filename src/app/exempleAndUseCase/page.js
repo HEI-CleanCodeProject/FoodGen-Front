ed0ab18e7e8d6f6f02867e5cfdd1cdcf7e481785
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-01-23 16:18:46 +0100

    feat: all the button style that we will use

---
+++ src/app/exempleAndUseCase/page.js
@@ -0,0 +1,9 @@
+export default function () {
+  return(
+    <div>
+      <p>this is exemple</p>
+      <button className="btn-app-yellow"> click me</button>
+      <button className="btn-app-dark"> click me</button>
+    </div>
+  )
+}
