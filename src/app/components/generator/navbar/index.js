09ae18faca4936359ae8759d24d690f00a9be35e
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-01-26 11:38:10 +0100

    feat: now the navigation bar is sticky

--- src/app/components/generator/navbar/index.js
+++ src/app/components/generator/navbar/index.js
@@ -3,7 +3,7 @@ import { Logo } from "./Logo"
 
 export default function Header() {
   return (
-    <header>
+    <header className="sticky top-0 w-full z-10">
       <nav className="mx-auto w-full bg-color-1 flex flex-row drop-shadow-xl" aria-label="navigation-bar for the generator page">
         <div className="w-5/6">
           <Logo />
