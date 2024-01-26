686bf142be2505b5990040cd6aa3f57b0e47482e
RyanIaro <hei.ryan.3@gmail.com>
2024-01-26 11:14:39 +0300

    feat: added a simple modal to user profile

--- src/app/components/generator/navbar/DarkZone.js
+++ src/app/components/generator/navbar/DarkZone.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { UserIcon } from "../../Icons";
 import styles from "./styles.module.css";
+import Modal from "../modal";
 
 export function DarkZone (){
+const [isOpen, setIsOpen ] = useState(false);
+  
   return(
     <div className="bg-color-2 w-1/6 relative color-yellow">
       <div className={`bg-color-2 h-full absolute ${styles["custom-block"]}`}></div>
       <div className="flex row color-1 p-1 h-full relative justify-end">
         <label className="self-center text-lg text-white">@userName</label>
-        <button className="px-4 rounded-full"><UserIcon /></button>
+        <button className="px-4 rounded-full" onClick={() => setIsOpen(true)}>
+          <UserIcon />
+        </button>
+        {isOpen && (
+        <Modal isOpen={isOpen} handleClose={() => setIsOpen(!isOpen)}>
+          <h1>Modal content</h1>
+        </Modal>
+        )}
       </div>
     </div>
   )
