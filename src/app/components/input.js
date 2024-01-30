0454e2c5260077e44e945ab61489a496813e9082
vohizy <hei.vohizy@gmail.com>
2024-01-30 11:02:51 +0300

    chore: add id for props in input

--- src/app/components/input.js
+++ src/app/components/input.js
@@ -1,11 +1,12 @@
 import React from "react";
-export default function Input({ name, type, placeholder }) {
+export default function Input({ name, type, placeholder, id }) {
   return (
     <>
       <input
         name={name}
         type={type}
         autoComplete={name}
+        id={id}
         required
         className="bg-gray-100 w-full text-sm px-4 py-3.5 rounded-md outline-[#333]"
         placeholder={placeholder}
