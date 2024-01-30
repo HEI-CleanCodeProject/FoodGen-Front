392733c2c5b5f780e6cb5ed60baa2e490b2b4705
vohizy <hei.vohizy@gmail.com>
2024-01-30 10:06:30 +0300

    feat: component for input

---
+++ src/app/components/input.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default function Input({ name, type, placeholder }) {
+  return (
+    <>
+      <input
+        name={name}
+        type={type}
+        autoComplete={name}
+        required
+        className="bg-gray-100 w-full text-sm px-4 py-3.5 rounded-md outline-[#333]"
+        placeholder={placeholder}
+      />
+    </>
+  );
+}
