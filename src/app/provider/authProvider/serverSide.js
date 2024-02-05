1b955c33a26fc4e6eb439993e227417d957a6b31
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-02-05 17:32:38 +0100

    feat: authentification now should work

--- src/app/provider/authProvider/serverSide.js
+++ src/app/provider/authProvider/serverSide.js
@@ -1,11 +1,12 @@
 import axios from "axios"
+import { cookies } from "next/headers";
 
 export const authProvider = {
-  getUser: async () => {
-    const token = sessionStorage.getItem(process.env.SESSION);
+  whoami: async () => {
+    const token = cookies.getItem(process.env.SESSION);
     return axios.get(process.env.NEXT_PUBLIC_BASE_URL,{
       headers:{
-        Authorization:token
+        Authorization:`Bearer ${token}`
       }
     })
   }
