e517473bb83b260fe5aacc6f177c135bfdc480ad
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-02-06 08:09:37 +0100

    feat: unit test for register page

--- src/app/(pages)/food/form/page.js
+++ src/app/(pages)/food/form/page.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "@/app/components/food/generator/navbar";
 import Search from "@/app/components/food/generator/searchBar/search";
 export default function Form(){
     return(
         <>
-        <Navbar/>
         <Search/>
         </>
         
