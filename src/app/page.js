4ab1b3706e06e7982699b076243ad5ec860787ba
njaina <hei.njaina.2@gmail.com>
2024-02-09 15:40:38 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -2,59 +2,42 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import getRandomMealImage from "./provider/assets/getImage";
 
 const HomePage = () => {
-  /*const [mealImage, setMealImage] = useState("");
-
-  useEffect(() => {
-    getRandomMealImage().then((imageURL) => {
-      setMealImage(imageURL);
-    });
-  }, []);*/
-
   return (
     <div className="min-h-screen main_bg">
       <nav className="_NavLink text-white">
-        <label className="_home_logo" htmlFor="homeLink">
-          <Image
-            src="/images/home.png"
-            alt="main image"
-            className=""
-            width={102}
-            height={102}
-          />
-        </label>
-
-        <Link id="homeLink" className="_link _hide" href="/">
-          Home
-        </Link>
-        <Link className="_link" href="/login">
-          Login
-        </Link>
-        <Link className="_link" href="/register">
-          Register
-        </Link>
+      <Image src="/images/home.png" className="_Logo" alt="logo" height={140} width={140}/>
+        <div className="rightSide">
+          <Link className="_link" href="/login">
+            Login
+          </Link>
+          <Link className="_link" href="/register">
+            Register
+          </Link>
+        </div>
       </nav>
+      
       <div className="text-white p-4 flex _left">
         <div className="">
           <div className="flex _left_flex">
             <Image
               src="/images/MenuBG.png"
               alt="logo"
-              width={600}
-              height={400}
-              className="_menu"
+              width={500}
+              height={500}
             />
             <div className="_Menu">
-              <div>
-                <p className="_textMenu">Welcoming</p>
+              <div className="m">
+                <div>
+                  <p className="_textMenu">Welcoming</p>
+                </div>
+                <img
+                  src="/images/MenuPointer.png"
+                  alt="logo"
+                  className="_pointer mouse"
+                />
               </div>
-              <img
-                src="/images/MenuPointer.png"
-                alt="logo"
-                className="_pointer mouse"
-              />
             </div>
           </div>
         </div>
