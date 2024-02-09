daf10211378872ccf3f94e9183b38123e722a0e7
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-02-09 14:53:32 +0100

    test: unit tests for generator page

--- __test__/login.test.js
+++ __test__/login.test.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import LoginPage from "@/app/(pages)/login/page";
+import { jest, expect} from "@jest/globals";
+import { authProvider } from "@/app/provider/authProvider/clientSide";
+
+jest.mock("@/app/provider/authProvider/clientSide");
 
 jest.mock('next/navigation', () => ({
   useRouter: () => ({
-    push: jest.fn(),
+    push:jest.fn(),
   }),
 }));
 
@@ -23,17 +27,23 @@ describe("LoginPage", () => {
 
   it("should submit form with correct data", () => {
     render(<LoginPage />);
-
-    const emailInput = screen.getByPlaceholderText("Email address");
-    const passwordInput = screen.getByPlaceholderText("Password");
-    const loginButton = screen.getByRole("button", { name: "Log in" });
+    jest.spyOn(authProvider,"login").mockImplementation(async () => "token");
+    const emailInput = screen.findByPlaceholderText("Email address");
+    const passwordInput = screen.findByPlaceholderText("Password");
+    const loginButton = screen.findByRole("button", { name: "Log in" });
 
     const email = "test@example.com";
     const password = "password123";
 
-    fireEvent.change(emailInput, { target: { value: email } });
-    fireEvent.change(passwordInput, { target: { value: password } });
-    fireEvent.click(loginButton);
-
+    const mockInput = [email, password];
+    Promise.all([emailInput,passwordInput]).then((each) => {
+      each.forEach((i, j) => {
+        fireEvent.change(i, { target: { value: mockInput[j]} });
+      })
+      loginButton.then((btn) => {
+        fireEvent.click(btn);
+        expect(authProvider.login).toBeCalled();
+      })
+    })
   });
 });
